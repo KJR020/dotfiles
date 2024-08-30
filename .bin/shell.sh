e142a7813c55616c94505dc74d8f5f7f534ffc46
KJR020 <johnjiro1114@gmail.com>
2024-08-31 08:20:36 +0900

    shell.shを更新

--- .bin/shell.sh
+++ .bin/shell.sh
@@ -10,6 +10,7 @@ setup_shell() {
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
         info "Installing oh-my-zsh..."
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     else
         info "oh-my-zsh is already installed."
     fi
