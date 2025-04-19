86ca47ec40d4370269aeba89deac66719e628871
KJR020 <johnjiro1114@gmail.com>
2025-04-20 08:52:58 +0900

    scriptを追加

---
+++ scripts/iso-now.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ISO Now
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author kojiro_kasada
+# @raycast.authorURL https://raycast.com/kojiro_kasada
+
+date '+%Y-%m-%dT%H:%M:%S+09:00'
